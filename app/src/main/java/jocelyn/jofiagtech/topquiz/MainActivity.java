ecac20b26d8ba5523506df460220f156a88c0a68
Jofiag <Aguemon.wahab.97@gmail.com>
2019-09-18 22:29:41 +0200

    Elément graphiques référencés.

--- app/src/main/java/jocelyn/jofiagtech/topquiz/MainActivity.java
+++ app/src/main/java/jocelyn/jofiagtech/topquiz/MainActivity.java
@@ -15,5 +15,9 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGreetingText = findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = findViewById(R.id.activity_main_name_input);
+        mPlayButton = findViewById(R.id.activity_main_play_btn);
     }
 }

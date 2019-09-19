06934cf8eb9e3838d5eb93fc1b4b61a0fb511677
Jofiag <Aguemon.wahab.97@gmail.com>
2019-09-19 11:02:05 +0200

    Désactivation du bouton de l'interface

--- app/src/main/java/jocelyn/jofiagtech/topquiz/MainActivity.java
+++ app/src/main/java/jocelyn/jofiagtech/topquiz/MainActivity.java
@@ -16,8 +16,12 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Référence des éléments graphiques
         mGreetingText = findViewById(R.id.activity_main_greeting_txt);
         mNameInput = findViewById(R.id.activity_main_name_input);
         mPlayButton = findViewById(R.id.activity_main_play_btn);
+
+        //Désactivation du bouton de l'interface
+        mPlayButton.setEnabled(false);
     }
 }

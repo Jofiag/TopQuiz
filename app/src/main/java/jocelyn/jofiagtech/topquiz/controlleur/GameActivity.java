6cbafff61edc65fc39b8bdda3e57beb67ba46ad7
Jofiag <Aguemon.wahab.97@gmail.com>
2019-09-21 13:45:03 +0200

    Questions générer et ajouter dans notre QuestionBank, ceci dans notre activité GameActivity.

--- app/src/main/java/jocelyn/jofiagtech/topquiz/controlleur/GameActivity.java
+++ app/src/main/java/jocelyn/jofiagtech/topquiz/controlleur/GameActivity.java
@@ -4,15 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+
 import jocelyn.jofiagtech.topquiz.R;
+import jocelyn.jofiagtech.topquiz.model.Question;
+import jocelyn.jofiagtech.topquiz.model.QuestionBank;
 
 public class GameActivity extends AppCompatActivity
 {
+    private QuestionBank mQuestionBank;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mQuestionBank = this.generateQuestions();
+    }
+
+    public QuestionBank generateQuestions()
+    {
+        Question question1 = new Question("Who is the creator of Android?",
+                Arrays.asList("Andy Rubin",
+                        "Steve Wozniak",
+                        "Jake Wharton",
+                        "Paul Smith"),
+                0);
+
+        Question question2 = new Question("When did the first man land on the moon?",
+                Arrays.asList("1958",
+                        "1962",
+                        "1967",
+                        "1969"),
+                3);
+
+        Question question3 = new Question("What is the house number of The Simpsons?",
+                Arrays.asList("42",
+                        "101",
+                        "666",
+                        "742"),
+                3);
+
+        return new QuestionBank(Arrays.asList(question1, question2, question3));
     }
 }
